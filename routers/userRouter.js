00ccbcac361d484bf7d54b60d2f85f1634789d20
hychoi <hayeong28@naver.com>
2020-02-14 02:54:02 +0900

    make userRouter, videoRouter (Init)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res, next) => res.send('user index'));
-userRouter.get("/edit", (req, res, next) => res.send('user edit'));
-userRouter.get("/password", (req, res, next) => res.send('user password'));
+userRouter.get(routes.home, (req, res, next) => res.send('Users'));
+userRouter.get(routes.userDetail, (req, res, next) => res.send('User Detail'));
+userRouter.get(routes.editProfile, (req, res, next) => res.send('Edit Profile'));
+userRouter.get(routes.changePassword, (req, res, next) => res.send('Change Password'));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.home, (req, res, next) => res.send("Videos"));
+videoRouter.get(routes.upload, (req, res, next) => res.send("Upload"));
+videoRouter.get(routes.videoDetail, (req, res, next) => res.send("Video Detail"));
+videoRouter.get(routes.editVideo, (req, res, next) => res.send("Edit Video"));
+videoRouter.get(routes.deleteVideo, (req, res, next) => res.send("Delete Video"));
 
 export default videoRouter;
