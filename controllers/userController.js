75786ee6a2be190d47f3463dff4e535dca17b7dd
hychoi <hayeong28@naver.com>
2020-02-19 23:24:12 +0900

    pass values to each template( render parameter object) & add pass code in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res, next) => res.render("join");
-export const login = (req, res, next) => res.render("login");
-export const logout = (req, res, next) => res.render("logout");
-export const users = (req, res, next) => res.render('users');
-export const userDetail = (req, res, next) => res.render('userDetail');
-export const editProfile = (req, res, next) => res.render('editProfile');
-export const changePassword = (req, res, next) => res.render('changePassword');
+export const join = (req, res, next) => res.render('join', {pageTitle: "Join"});
+export const login = (req, res, next) => res.render('login', {pageTitle: "Login"});
+export const logout = (req, res, next) => res.render('logout', {pageTitle: "Logout"});
+export const users = (req, res, next) => res.render('users', {pageTitle: "Users"});
+export const userDetail = (req, res, next) => res.render('userDetail', {pageTitle: "UserDetail"});
+export const editProfile = (req, res, next) => res.render('editProfile', {pageTitle: "EditProfile"});
+export const changePassword = (req, res, next) => res.render('changePassword', {pageTitle: "ChangePassword"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res, next) => res.render("home")
-export const search = (req, res, next) => res.render("search");
-export const video = (req, res, next) => res.render("videos");
-export const upload = (req, res, next) => res.render("upload");
-export const videoDetail = (req, res, next) => res.render("videoDetail");
-export const editVideo = (req, res, next) => res.render("editVideo");
-export const deleteVideo = (req, res, next) => res.render("deleteVideo");
+export const home = (req, res, next) => res.render("home", {pageTitle: "Home"})
+export const search = (req, res, next) => res.render("search", {pageTitle: "Search"});
+export const video = (req, res, next) => res.render("videos", {pageTitle: "Video"});
+export const upload = (req, res, next) => res.render("upload", {pageTitle: "Upload"});
+export const videoDetail = (req, res, next) => res.render("videoDetail", {pageTitle: "VideoDetail"});
+export const editVideo = (req, res, next) => res.render("editVideo", {pageTitle: "EditVideo"});
+export const deleteVideo = (req, res, next) => res.render("deleteVideo", {pageTitle: "DeleteVideo"});
