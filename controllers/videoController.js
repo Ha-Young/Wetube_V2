efa81d76c9284c43c8a9ea3a607f3fd1d2f933c6
hychoi <hayeong28@naver.com>
2020-02-19 23:44:53 +0900

    write search controller in videoController how to get form get method name value. the way is req.query

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,20 @@
 export const home = (req, res, next) => res.render("home", {pageTitle: "Home"})
-export const search = (req, res, next) => res.render("search", {pageTitle: "Search"});
+
+export const search = (req, res, next) => {
+    const {
+        query : {
+            term : searchingBy
+        }
+    } = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+
+}
 export const video = (req, res, next) => res.render("videos", {pageTitle: "Video"});
+
 export const upload = (req, res, next) => res.render("upload", {pageTitle: "Upload"});
-export const videoDetail = (req, res, next) => res.render("videoDetail", {pageTitle: "VideoDetail"});
-export const editVideo = (req, res, next) => res.render("editVideo", {pageTitle: "EditVideo"});
-export const deleteVideo = (req, res, next) => res.render("deleteVideo", {pageTitle: "DeleteVideo"});
+
+export const videoDetail = (req, res, next) => res.render("videoDetail", {pageTitle: "Video Detail"});
+
+export const editVideo = (req, res, next) => res.render("editVideo", {pageTitle: "Edit Video"});
+
+export const deleteVideo = (req, res, next) => res.render("deleteVideo", {pageTitle: "Delete Video"});
