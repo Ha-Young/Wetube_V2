dc459d6204e638dfa2b1ba220bbf2916659c36d6
hychoi <hayeong28@naver.com>
2020-02-19 21:55:13 +0900

    view engine pug seet and init

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res, next) => res.send("Join");
-export const login = (req, res, next) => res.send("Login");
-export const logout = (req, res, next) => res.send("Logout");
-export const users = (req, res, next) => res.send('Users');
-export const userDetail = (req, res, next) => res.send('User Detail');
-export const editProfile = (req, res, next) => res.send('Edit Profile');
-export const changePassword = (req, res, next) => res.send('Change Password');
+export const join = (req, res, next) => res.render("join");
+export const login = (req, res, next) => res.render("login");
+export const logout = (req, res, next) => res.render("logout");
+export const users = (req, res, next) => res.render('users');
+export const userDetail = (req, res, next) => res.render('userDetail');
+export const editProfile = (req, res, next) => res.render('editProfile');
+export const changePassword = (req, res, next) => res.render('changePassword');
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res, next) => res.send("Home");
-export const search = (req, res, next) => res.send("Search");
-export const video = (req, res, next) => res.send("Videos");
-export const upload = (req, res, next) => res.send("Upload");
-export const videoDetail = (req, res, next) => res.send("Video Detail");
-export const editVideo = (req, res, next) => res.send("Edit Video");
-export const deleteVideo = (req, res, next) => res.send("Delete Video");
+export const home = (req, res, next) => res.render("home")
+export const search = (req, res, next) => res.render("search");
+export const video = (req, res, next) => res.render("videos");
+export const upload = (req, res, next) => res.render("upload");
+export const videoDetail = (req, res, next) => res.render("videoDetail");
+export const editVideo = (req, res, next) => res.render("editVideo");
+export const deleteVideo = (req, res, next) => res.render("deleteVideo");
