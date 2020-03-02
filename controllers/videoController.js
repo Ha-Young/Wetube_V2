f8787ed7e09939365273a1e052243eb251765676
hychoi <hayeong28@naver.com>
2020-03-02 22:06:48 +0900

    get Videos in Database mongoDB use async await keyword and plus try catch statement

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,16 @@
 import routes from "../routes"
+import Video from "../models/Video"
 
-export const home = (req, res, next) => 
-{
-    res.render("home", {pageTitle: "Home", videos})
+export const home = async(req, res, next) => {
+    try {
+        const videos = await Video.find({});
+        res.render("home", {pageTitle: "Home", videos});
+    } catch(error) {
+        console.log(error);
+        res.render("home", {pageTitle:"Home", videos:[]})
+    }
 }
+
 export const search = (req, res, next) => {
     const {
         query : {
@@ -12,6 +19,7 @@ export const search = (req, res, next) => {
     } = req;
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
+
 export const video = (req, res, next) => res.render("videos", {pageTitle: "Video"});
 
 export const getUpload = (req, res, next) => 
