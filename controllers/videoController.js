d6f4bdea57a0adb3033a4802745ed30af94843b3
hychoi <hayeong28@naver.com>
2020-03-03 00:18:01 +0900

    video upload and edit video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -55,8 +55,48 @@ export const postUpload = async (req, res) => {
     
 }
 
-export const videoDetail = (req, res, next) => res.render("videoDetail", {pageTitle: "Video Detail"});
+export const videoDetail = async (req, res, next) => {
+    const {
+        params:{id}
+    } = req;
+    try{
+        const video = await Video.findById(id);
+        res.render("videoDetail", {pageTitle: "Video Detail", video});
+    }catch(error){
+        console.log(error);
+        res.redirect(routes.home);
+    }
+    
+}
 
-export const editVideo = (req, res, next) => res.render("editVideo", {pageTitle: "Edit Video"});
+export const getEditVideo = async(req, res, next) => {
+    const {
+        params:{id}
+    } = req;
+
+    try {
+        const video = await Video.findById(id);
+        res.render("editVideo", {pageTitle: `Edit ${video.title}`, video});
+    } catch(error) {
+        console.log(error);
+        res.redirect(routes.home);
+    }
+
+}
+
+export const postEditVideo = async(req, res) => {
+    const {
+        params:{id},
+        body:{title, description}
+    } = req;
+
+    try {
+        await Video.findOneAndUpdate({_id:id}, {title, description});
+        res.redirect(routes.videoDetail(id));
+    }catch(error) {
+        console.log(error);
+        res.redirect(routes.home);
+    }
+}
 
 export const deleteVideo = (req, res, next) => res.render("deleteVideo", {pageTitle: "Delete Video"});
