b831af3eacbab273db5e6db2d954c3a7f2a51b27
hychoi <hayeong28@naver.com>
2020-03-12 14:24:30 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
-    "extends": [
-        "airbnb-base",
-        "plugin:prettier/recommended"
-    ]
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base','plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
